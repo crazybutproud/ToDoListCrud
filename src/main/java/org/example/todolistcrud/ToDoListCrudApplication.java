86bacdb1a6221fcb5703775ee4c0b4f6de3b6bd6
crazybutproud <ms.terebonk@mail.ru>
2025-06-19 23:33:24 +0300

    Start the project

---
+++ src/main/java/org/example/todolistcrud/ToDoListCrudApplication.java
@@ -0,0 +1,13 @@
+package org.example.todolistcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/todolistcrud/ToDoListCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todolistcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
