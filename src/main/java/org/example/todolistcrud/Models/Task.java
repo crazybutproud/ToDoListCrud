36e37de793cbbab812287a3dd89310de72aa008a
crazybutproud <ms.terebonk@mail.ru>
2025-06-23 20:30:36 +0300

    Add @

--- src/main/java/org/example/todolistcrud/Models/Task.java
+++ src/main/java/org/example/todolistcrud/Models/Task.java
@@ -1,27 +1,25 @@
 package org.example.todolistcrud.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-public class Task { // зависимости???
+@Table(name="appointment")
+public class Task {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "title", length = 50, nullable = false)
     private String title;
+    @Column(name = "description", length = 200, nullable = false)
     private String taskDescription;
+    @Column(name = "completed")
     private boolean completed;
 
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
 
     @Override
     public String toString() {
